7fde300fe8e27345ed193569dd93846d59d59625
Christine Schale <108648777+Ranunculus32@users.noreply.github.com>
2023-01-17 22:46:28 +0100

    Landing page 90%
    welcome.js and css are now fully functional:
    
    -linked to Adventure.css (Play btn), Login and Signup.
    -Welcome.css needs minor changes with button:hover and responsiveness with the header.
    
    The functionality is achieved through changing the App.js and route the links.
    
    ***Attempted to import Font Awesome, but it is too much plugins for just a button
    **** Play button is now changed to ordinary button shaped: tried importing the btn from figma as img, as well as manipulating the container div to be a triangle.
    ***NEW DISCOVERY
    To remove the line under the text when <Link to=''> is used, simply put the entire div inside the tag.

--- game-project/src/components/Welcome.js
+++ game-project/src/components/Welcome.js
@@ -1,22 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Welcome.css";
 
 function Welcome() {
   return (
     <div className="main-box">
-      <div className="play-btn"> Play </div>
-      <div className="login-btn">Login</div>
-      <div className="signup-btn">Signup</div>
-      <div className="statement-box">
-        <h2> Going through The Enchanter's Forest</h2>
-        <p> You will explore the mysterious world of legends. </p>
-        <p>
-          This is where your courage and strength will be tested. And where you
-          will discover the value of your decisions in the journey. This is
-          where your courage and strength will be tested. And where you will
-          discover the value of your decisions in the journey.
-        </p>
-        <p className="highlight-p">Choose your path wisely, brave knight!</p>
+      <div className="header d-flex ">
+        <Link to="/Adventure">
+          <div className="play-btn btn ">Play</div>
+        </Link>
+        <Link to="/Login">
+          <div className="login-btn btn ">Login</div>
+        </Link>
+        <Link to="/Signup">
+          <div className="signup-btn btn ">Signup</div>
+        </Link>
+      </div>
+      <div className="wrapper-statement-box">
+        <div className="statement-box card">
+          <div className="card-text">
+            <h2> Going through The Enchanter's Forest</h2>
+
+            <p> You will explore the mysterious world of legends. </p>
+            <p>
+              This is where your courage and strength will be tested. Where you
+              will discover the value of your decisions in the journey.
+            </p>
+            <p className="highlight-p">
+              Choose your path wisely, brave knight!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
