165756b8630af40b9c2cbe2fb5ccb0b1552ac2ff
Christine Schale <108648777+Ranunculus32@users.noreply.github.com>
2023-01-17 14:11:31 +0100

    Welcome.js & css
    Welcome.js created as landing page
    Styling welcome.js

---
+++ game-project/src/components/Welcome.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/Welcome.css";
+
+function Welcome() {
+  return (
+    <div className="main-box">
+      <div className="play-btn"> Play </div>
+      <div className="login-btn">Login</div>
+      <div className="signup-btn">Signup</div>
+      <div className="statement-box">
+        <h2> Going through The Enchanter's Forest</h2>
+        <p> You will explore the mysterious world of legends. </p>
+        <p>
+    
+          This is where your courage and strength will be tested. And where you
+          will discover the value of your decisions in the journey.{" "}
+        </p>
+        <p className="highlight-p">Choose your path wisely, brave knight!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
